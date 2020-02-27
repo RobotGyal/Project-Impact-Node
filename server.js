10a338e8e575504c6c26e95b9fac9416f2fa8e89
aleia <student@users-MacBook-Pro.local>
2020-02-26 18:07:14 -0800

    can read data from dataset

--- server.js
+++ server.js
@@ -1,8 +1,32 @@
 const express = require('express')
 const app = express();
+// // const pandas = require('pandas')
+// const pd = require('pandas-js')
+// // const z = require('zebras')
+
+
+// // function read_meteor_data(file){
+// //   pd.read_csv(file)
+// // }
+// const data = pd.read_csv('api_meteor_data.csv')
+// // const data = z.readCSV('api_meteor_data.csv')
+
+const csv = require('csv-parser');
+const fs = require('fs');
+var data = []
+fs.createReadStream('api_meteor_data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    // console.log(row);
+    data.push(row)
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World! I\'m working')
+  res.send(data)
 });
 
 app.listen(8000, () => {
